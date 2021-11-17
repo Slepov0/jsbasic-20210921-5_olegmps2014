b797863fe226f7c6bd825ef20ebf5b9c6d7f131d
OVSlepov <O.Slepov@hyundai-autoever.ru>
2021-11-17 16:46:22 +0300

    added task #1 solution 

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  friends.forEach((value, index, array) => {
+    let li = document.createElement('li');
+    li.innerHTML+=value.firstName+' '+value.lastName;
+    ul.appendChild(li);
+  })
+  return ul;
 }
